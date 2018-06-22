94854c5e0d378baf40215c8f3112f41f4592d68a
allierowan <allie.rowan@mapbox.com>
2018-06-22 11:27:47 -0400

    update scripts

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,31 +7,34 @@ echo "node version is:"
 which node
 node -v
 
-if [[ ${PUBLISH} == 'On' ]]; then
-    echo "PUBLISH is set to '${PUBLISH}', publishing!"
-    NPM_FLAGS=''
-    if [[ ${BUILD_TYPE} == "Debug" ]]; then
-        NPM_FLAGS='--debug'
-    fi
 
-    echo "dumping binary meta..."
-    ./node_modules/.bin/node-pre-gyp reveal $NPM_FLAGS
-
-    # enforce that binary has proper ORIGIN flags so that
-    # it can portably find libtbb.so in the same directory
-    if [[ $(uname -s) == 'Linux' ]]; then
-        readelf -d ./lib/binding/node_valhalla.node > readelf-output.txt
-        if grep -q 'Flags: ORIGIN' readelf-output.txt; then
-            echo "Found ORIGIN flag in readelf output"
-            cat readelf-output.txt
-        else
-            echo "*** Error: Could not found ORIGIN flag in readelf output"
-            cat readelf-output.txt
-            exit 1
-        fi
-    fi
-
-    ./node_modules/.bin/node-pre-gyp package publish info $NPM_FLAGS
+if [[ $(is_pr_merge) ]]; then
+      echo "Skipping publishing because this is a PR merge commit"
 else
-    echo "PUBLISH is set to '${PUBLISH}', skipping."
+      echo "Commit message: ${COMMIT_MESSAGE}"
+
+      if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
+          echo "Publishing"
+          ./node_modules/.bin/node-pre-gyp package publish $@
+      elif [[ ${COMMIT_MESSAGE} =~ "[republish binary]" ]]; then
+          echo "Re-Publishing"
+          ./node_modules/.bin/node-pre-gyp package unpublish publish $@
+      else
+          echo "Skipping publishing since we did not detect either [publish binary] or [republish binary] in commit message"
+      fi
 fi
+
+# if [[ ${PUBLISH} == 'On' ]]; then
+#     echo "PUBLISH is set to '${PUBLISH}', publishing!"
+#     NPM_FLAGS=''
+#     if [[ ${BUILD_TYPE} == "Debug" ]]; then
+#         NPM_FLAGS='--debug'
+#     fi
+#
+#     echo "dumping binary meta..."
+#     ./node_modules/.bin/node-pre-gyp reveal $NPM_FLAGS
+#
+#     ./node_modules/.bin/node-pre-gyp package publish info $NPM_FLAGS
+# else
+#     echo "PUBLISH is set to '${PUBLISH}', skipping."
+# fi
